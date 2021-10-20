829ca0dd6d50db8732fae1106c68ea7567593814
Loctryl <letallec.thibautl@gmail.com>
2021-10-20 10:38:08 +0200

    Oui

--- puiss4.c
+++ puiss4.c
@@ -25,22 +25,21 @@ void disp(void){
     }
     printf(" |");
   }
-  printf("\n  - - - - - - - ");
+  printf("\n  1 2 3 4 5 6 7 ");
 }
 
 int statusgame(void){
   int count = 0;
   int align = 0;
+  printf("\nj'uis dans le status de la game\n");
 
   //game won
 
   if (align == 4){
     return 2 ; 
   }
-
   for(int l=0; l<NBL; l++) {
     for(int c=0; c<NBC; c++) {
-      printf("%c",tab[l][c]);
       if (tab[l][c] != '.'){
         count ++;
       }
@@ -55,17 +54,23 @@ int statusgame(void){
 void main(void) {
   // Init du jeu:
   init();
+  printf("\nFin d'init\n");
   disp();
+  printf("\nFin de disp\n");
   // Boucle du jeu:
   while(statusgame() == 0){
+    printf("\nJ'uis rentré dans la boucle\n");
     int colonne, code_retour;
     int i = 7 ;
     do {
+      printf("\nOn joue dans quel colonne ?\n");
       code_retour = scanf("%d", &colonne);
     } while (0<code_retour<8);{
+        printf("j'uis dans celle là ?");
         while (tab[i][code_retour] != '.'){
           i --;
         }
+        printf("Et est-ce que j'en sors ?");
         tab[i][code_retour] = token[joueur];
     }
   }
