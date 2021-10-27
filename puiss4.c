37ef975bfbc919465f0c112f7e36e1c20edffc4c
Loctryl <letallec.thibautl@gmail.com>
2021-10-27 11:54:01 +0200

    Last update

--- puiss4.c
+++ puiss4.c
@@ -2,13 +2,12 @@
 #define NBL 6
 #define NBC 7
 
-
 char tab[NBL][NBC];
 char token [] = "ox";
-int joueur = 0;
+int player = 1;
 int count = 0;
 
-void init(void) {
+void Init(void) {
   for(int l=0; l<NBL; l++) {
     for(int c=0; c<NBC; c++) {
       tab[l][c] = '.';
@@ -16,7 +15,7 @@ void init(void) {
   }
 }
 
-void disp(void){
+void Disp(void){
 
   printf("  _ _ _ _ _ _ _ ");
   for(int l=0; l<NBL; l++) {
@@ -29,72 +28,130 @@ void disp(void){
   printf("\n  1 2 3 4 5 6 7 \n");
 }
 
-int statusgame(){
-  printf("Je rentre dans la status de la game\n");
-  int align = 0;
+void Empty(void){
+  int c = getchar();
+  while (c != '\n' && c != EOF){
+    c = getchar();
+  }
+}
+
+int Verif(int range){
+  int save;
+  int answer;
+  for (;;) {
+    save = scanf (" %d", &answer);
+
+    if (save == EOF) {
+      fputs ("You didn't write anything...\n", stderr);
+      return 0;
+    }
+
+    else if (save == 0) {
+      fputs ("Could you choose an intager please ? ...\n", stderr);
+      Empty();
+    }
+
+    else if (answer < 1 || range < answer) {
+      fputs ("This is not a choice...\n", stderr);
+      Empty();
+    }
+    
+    else {  
+      Empty();
+      return answer;
+    }
+  }
+}
+
+int StatusGame(){
+
   if (count>6){
     for(int l=0; l<NBL; l++) {
       for(int c=0; c<5; c++) {
-        if (tab[l][c] == token[joueur] && tab[l][c+1] == token[joueur] && tab[l][c+2] == token[joueur] && tab[l][c+3] == token[joueur]){
-          printf("Je suis 1");
+        if (tab[l][c] == token[player] && tab[l][c+1] == token[player] && tab[l][c+2] == token[player] && tab[l][c+3] == token[player]){
+          printf("\nPlayer %d won !! \n", player+1);
           return 1;
         } 
       }  
     }
+
     for(int c=0; c<NBC; c++) {
       for(int l=0; l<4; l++) {
-        if (tab[l][c] == token[joueur] && tab[l+1][c] == token[joueur] && tab[l+2][c] == token[joueur] && tab[l+3][c] == token[joueur]){
-          printf("Je suis 2");
+        if (tab[l][c] == token[player] && tab[l+1][c] == token[player] && tab[l+2][c] == token[player] && tab[l+3][c] == token[player]){
+          printf("\nPlayer %d won !! \n", player+1);
           return 1;
         } 
       }
     }
+
     for(int c=6; c>2; c--) {
       for(int l=0; l<2; l++) {
-        if (tab[l][c] == token[joueur] && tab[l+1][c-1] == token[joueur] && tab[l+2][c-2] == token[joueur] && tab[l+3][c-3] == token[joueur]){
-          printf("Je suis 3");
+        if (tab[l][c] == token[player] && tab[l+1][c-1] == token[player] && tab[l+2][c-2] == token[player] && tab[l+3][c-3] == token[player]){
+          printf("\nPlayer %d won !! \n", player+1);
           return 1;
         } 
       }
     }
+
     for(int c=0; c<3; c++) {
       for(int l=0; l<2; l++) {
-        if (tab[l][c] == token[joueur] && tab[l+1][c+1] == token[joueur] && tab[l+2][c+2] == token[joueur] && tab[l+3][c+3] == token[joueur]){
-          printf("Je suis 4");
+        if (tab[l][c] == token[player] && tab[l+1][c+1] == token[player] && tab[l+2][c+2] == token[player] && tab[l+3][c+3] == token[player]){
+          printf("\nPlayer %d won !! \n", player+1);
           return 1;
         } 
       }
     }
   }
-  if (count == 42){
+
+  if (count >= 42){
+    printf("\nThe game is full ! Game Over !\n");
     return 1;
   }
   return 0;
 }
 
 void main(void) {
-    // Init du jeu:
-    init();
-    disp();
-    // Boucle du jeu:
-    while(statusgame() == 0){
-      int colonne;
-      int i = 5 ;
-      printf("\nOn joue dans quel colonne ?\n");
-      scanf("%d", &colonne);
-      if (colonne<=7 && tab[6][colonne] != '.'){
-        while (tab[i][colonne-1] != '.'){
-          i --;
+  int replay = 1;
+  while (replay == 1){
+
+    Init();
+    Disp();
+
+    while(StatusGame() == 0){
+
+      int column, i=5;
+      player = !player;
+      printf("\nPlayer %d is playing ! \nWhich column do you choose ?\n",player+1);
+      column = Verif(7);
+
+      while(tab[0][column-1] != '.'){
+        printf("\nThis column is full, choose an other one please.\nWhich column do you choose ?\n");
+        column = Verif(7);
         }
-        tab[i][colonne-1] = token[joueur];
-        disp();
-        printf("Changement de joueur ! ");
-        joueur = !joueur;
-        count ++;
-        printf("%d \n", count);
-      } else {
-        printf("\nVous ne pouvez pas jouer dans cette colonne ! \n");
-        disp();
-        }
+
+      while (tab[i][column-1] != '.'){
+        i --;
+      }
+
+      tab[i][column-1] = token[player];
+      Disp();
+      count ++;
+      printf("---------------------\n");
     }
+
+    replay = 0;
+    printf("\nDo you want to play again ?\n1 for Yes\n2 for No\n");
+    replay = Verif(2);
+
+    while (replay != 1 && replay != 2){
+      printf("\nDo you want to play again ?\n1 for Yes\n2 for No\n");
+      replay = Verif(2);
+    }
+
+    if (replay == 1){
+      printf("\nAh shit ... here we go again..\n");
+    }
+    count = 0;
+  }
+  printf("\nGame Over ! See You !\n\n");
 }
